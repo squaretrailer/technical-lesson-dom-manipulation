390388ceb5df7786badc98a1ec7c16abc572ab04
Christian <your_email@example.com>
2026-04-10 08:04:40 +0300

    feat: code updated

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // Write your code here!
 
+// STep 1
+const userInfo = {
+    username: "flatUser123",
+    description: "Hi, welcome to my webpage! Just a new user of this website!",
+    theme: "red"
+}
+
+function displayUser(userInfo){
+    const header = document.querySelector("#main")
+    header.textContent = userInfo.username + "'s Personal Webpage"
+    header.className = userInfo.theme
+
+    const description = document.createElement("p")
+    const body = document.querySelector("body")
+
+   body.append(description)
+}
+
+displayUser(userInfo)
+
